896a7fd0e53cbefa98b3a551bfa5015942672314
komiyamma <komiyama0629@gmail.com>
2024-12-01 14:59:05 +0900

    iphoneなどの写真のHEICをPNGに変換する。画像を右クリックしてHEIC→PNGを選択すると変換できるように。主に、Twitterなどの写真を上げる用途。全コンバート用途ではない。

---
+++ HEIC2PNG.bat
@@ -0,0 +1,3 @@
+C:
+cd "C:\usr\convert_heic_to_png"
+"C:\usr\convert_heic_to_png\convert_heic_to_png.exe" "%~1"
---
+++ src/convert_heic_to_png/convert_heic_to_png/Program.cs
@@ -0,0 +1,30 @@
+﻿using ImageMagick;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string? targetDir = Path.GetDirectoryName(args[0]);
+        if (targetDir == null)
+        {
+            return;
+        }
+
+        string? targetFile = Path.GetFileNameWithoutExtension(args[0]);
+        if (targetFile == null)
+        {
+            return;
+        }
+
+        string inputPath = args[0];
+        string outputPath = Path.Combine(targetDir, targetFile + ".png");
+
+        using (var image = new MagickImage(inputPath))
+        {
+            // image.Resize(output_width, output_height); // 幅と高さを50%に縮小
+            image.Write(outputPath);
+        }
+
+        Console.WriteLine("変換完了: " + outputPath);
+    }
+}
