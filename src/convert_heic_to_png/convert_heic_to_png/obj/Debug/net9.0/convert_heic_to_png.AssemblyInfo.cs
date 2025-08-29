e695686122aa47197524d6c6e200e1bf4bfa66db
komiyamma <komiyama0629@gmail.com>
2025-08-29 19:21:37 +0900

    chore: 不要なファイルを削除し、プロジェクト設定を更新

--- src/convert_heic_to_png/convert_heic_to_png/obj/Debug/net9.0/convert_heic_to_png.AssemblyInfo.cs
+++ src/convert_heic_to_png/convert_heic_to_png/obj/Debug/net9.0/convert_heic_to_png.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("convert_heic_to_png")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6002a2df65f34c13cfcdfc41ffa176e877e6bf3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("convert_heic_to_png")]
 [assembly: System.Reflection.AssemblyTitleAttribute("convert_heic_to_png")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
